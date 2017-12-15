4cbdbff20b2ff05ed598a47a9171a423b9d66df3
UIdoer <xian-zi-ming@qq.com>
2017-12-15 23:38:47 +0800

    nav优化

--- js/pc.js
+++ js/pc.js
@@ -24,18 +24,31 @@ $(function(){
     //同时切换菜单的显示与隐藏
 
     $('.menu-btn').click(function(){
-        $(this).toggleClass('close');
         if(!$('.link').hasClass('show')){
+            $(this).addClass('close').removeClass('open');
             var tmp_nav = $('<div>').addClass('tmp-nav').bind('click',function(event){
                 event.stopPropagation();
                 $('.menu-btn').before($('.link').removeClass('show'));
                 $('.tmp-nav').remove();
+                $('.close').removeClass('close').addClass('open')
             }).appendTo('body');
             $('.link').addClass('show').appendTo('body');
         }else{
+                $(this).addClass('open').removeClass('close');
                 $('.menu-btn').before($('.link').removeClass('show'));
                 $('.tmp-nav').remove();
         }
     });
+
+    //如果窗口大于或等于992，那么将菜单放至menu-btn之前
+    //同时移除tmp-nav，也将menu-btn的close这个类移除
+    $(window).resize(function(){
+        if( $(this).width()>= 992 ){
+            $('.menu-btn').before($('.link').removeClass('show'));
+            $('.tmp-nav').remove();
+            $('.close').removeClass('close')
+        }
+    });
+
 
 });
