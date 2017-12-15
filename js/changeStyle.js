7bdba05b75a50a1ffbafa95e2618e5298ad3d71f
UIdoer <xian-zi-ming@qq.com>
2017-12-15 09:45:46 +0800

    pc端样式更新

---
+++ js/changeStyle.js
@@ -0,0 +1,17 @@
+function IsPC(){
+ var userAgentInfo = navigator.userAgent;
+ var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+ var flag = true;
+ for (var v = 0; v < Agents.length; v++) {
+     if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
+ }
+ return flag;
+}
+var styleLinke = document.getElementById('style');
+var head = document.getElementsByTagName('head')[0]
+if(IsPC()){
+    styleLinke.setAttribute('href','css/pc.css');
+}else{
+    styleLinke.setAttribute('href','css/mobile.css');
+}
+head.appendChild(styleLinke);
---
+++ js/pc.js
@@ -0,0 +1,22 @@
+$(function(){
+    // alert('引入成功！');
+
+    // banner
+    var swiper = new Swiper('.swiper-container',{
+        spaceBetween: 30,
+        speed:200,
+        loop:true,
+        pagination:{
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+        slidesPerView : 1,
+        spaceBetween : 0,
+        autoplay: {
+            delay: 3000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+        },
+        autoplayDisableOnInteraction : false,
+    });
+});
