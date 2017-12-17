733bff2dcaef116a40e110710e1bb0eba0cf1c89
UIdoer <xian-zi-ming@qq.com>
2017-12-17 13:04:06 +0800

    f

--- js/changeStyle.js
+++ js/changeStyle.js
@@ -1,6 +1,6 @@
 function IsPC(){
  var userAgentInfo = navigator.userAgent;
- var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPod");
+ var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone","iPad", "iPod");
  var flag = true;
  for (var v = 0; v < Agents.length; v++) {
      if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
--- js/mobile.js
+++ js/mobile.js
@@ -1,3 +1,31 @@
 $(function(){
-    // alert('引入成功！');
+     // 窗口滚动条 scroll style
+    $('html').niceScroll({
+        ccursorcolor:"#444",
+        background:'rgba(0,0,0,0.2)',
+        cursoropacitymax:1,
+        touchbehavior:false,
+        cursorwidth:"8px",
+        cursorborder:"0",
+        cursorborderradius:"8px"
+    });
+
+    // banner
+    var swiper = new Swiper('.swiper-container',{
+        spaceBetween: 30,
+        speed:200,
+        loop:true,
+        pagination:{
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+        slidesPerView : 1,
+        spaceBetween : 0,
+        autoplay: {
+            delay: 3000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+        },
+        autoplayDisableOnInteraction : false,
+    });
 });
