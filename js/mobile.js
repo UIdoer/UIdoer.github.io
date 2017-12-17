aefe267aed42af465f8b76bea7c7bc2070779c4f
UIdoer <xian-zi-ming@qq.com>
2017-12-17 17:45:07 +0800

    af

--- js/mobile.js
+++ js/mobile.js
@@ -7,7 +7,7 @@ $(function(){
         touchbehavior:false,
         cursorwidth:"8px",
         cursorborder:"0",
-        cursorborderradius:"8px"
+        cursorborderradius:"8px",
     });
 
     // banner
@@ -28,6 +28,70 @@ $(function(){
         },
         autoplayDisableOnInteraction : false,
     });
+    var winW,winH;
+    function getWinSize(){
+        winW = $(window).width();
+        winH = $(window).height();
+    }
+    getWinSize();
+    $(window).resize(function(){
+        getWinSize();
+    });
+    function org0(){
+        $('.link').removeClass('hidden-xs hidden-sm fr').css({
+                zIndex:999999,
+                width:'70vw',
+                height:'100vh !important',
+                margin:0,
+                background:'red',
+                position:'fixed',
+                top:0,
+                right:-winW * 0.6,
+            }).appendTo('body')
+            $('.header').css({
+                zIndex:9999,
+                width:'100vw',
+            });
+            $('.page').css({
+                zIndex:999,
+                width:'100vw',
+                position:'relative',
+                top:0,
+                right:0,
+                background:'#fff'
+            })
 
+            // alert('竖')
+    }
+
+    function org90(){
+        $('.menu-btn').before($('.link').addClass('hidden-xs hidden-sm fr').removeAttr('style'));
+        $('.header .page').removeAttr('style')
+        // alert('横')
+    }
+
+    if( window.orientation == 90 ){
+        org90();
+    }else if( window.orientation == 0 ){
+        org0();
+    }
+    window.addEventListener("orientationchange", function() {
+        if( window.orientation == 90 ){
+            org90();
+        }else if( window.orientation == 0 ){
+            org0();
+        }
+    }, false);
+
+    $(window).click(function(){
+        // alert('click window');
+        $('.header,.page').animate({
+            marginLeft: - winW * 0.7
+        },200);
+        $('.link').animate({
+            right:0
+        },200);
+        $('.mask').show();
+    })
 
 });
