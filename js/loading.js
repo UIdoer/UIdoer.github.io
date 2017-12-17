dca7e17dd33d71dd86d2d1bf48a446b891095874
UIdoer <xian-zi-ming@qq.com>
2017-12-17 15:12:16 +0800

    s

--- js/loading.js
+++ js/loading.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
   var loading = document.getElementById('loading');
   setTimeout(function(){
         loading.style.display = 'none'
-  },1200);
+  },1500);
 });
