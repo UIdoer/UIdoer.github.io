ced6ce171fb43a2e865a040e34a4ea61cae7af46
UIdoer <xian-zi-ming@qq.com>
2017-12-19 11:04:34 +0800

    ff

--- demo/js/index.js
+++ demo/js/index.js
@@ -8,41 +8,45 @@ $(function(){
         winW = $(window).width();
         asideWidth = winW * 0.7;
     });
-    // var swiptw = $(".page").width() - $(".borrowlistcon .block").eq(0).width();
     var pageLeft = parseFloat( $('#page').css('left') );
     var menuLeft = parseFloat( $('#menu').css('left') );
     var LorR = null;
-    var num = 0 ;
+    //menu 收起
+    function hiddenMenu(){
+         $('#page,#mask').animate({
+            left:'0'
+        },100);
+        $('#mask').animate({background:'rgba(0,0,0,0)'},100).css({display:'none'});
+    }
+    //menu 展开
+    function showMenu(){
+        $('#page,#mask').animate({
+            left:'-70vw'
+        },100);
+        $('#menu').animate({
+            left:'30vw'
+        },100);
+    }
     $('#page,#menu').swipe({
         swipeStatus:function(event, phase, direction, distance, duration, fingerCount){
                 console.log(event.type);
             if( event.type == 'touchmove' &&  direction == 'left'&& distance < asideWidth ){
                 LorR = 'left';
-                num = 1;
                 $('#page,#mask').css({
                     left:pageLeft - distance
                 });
                 $('#menu').css({
-                    left:menuLeft - distance * ( 2 - distance / asideWidth )
+                    left:menuLeft - distance /2
                 });
                 $('#mask').css({display:'block',background:'rgba(0,0,0,'+0.8 * distance / asideWidth+')'});
             }else if( event.type == 'touchmove' &&  direction == 'right'&& distance < asideWidth ){LorR = 'right';}
 
             if(event.type == 'touchend' ){
 
-                console.log(num);
                 if(LorR == 'left'){
-                    $('#page,#mask').animate({
-                        left:'-70vw'
-                    },100);
-                    $('#menu').animate({
-                        left:'30vw'
-                    },100);
+                    showMenu()
                 }else if(LorR == 'right'){
-                    $('#page,#mask').animate({
-                        left:'0'
-                    },100);
-                    $('#mask').animate({background:'rgba(0,0,0,0)'},100).css({display:'none'});
+                   hiddenMenu()
                 }
 
             }
@@ -51,11 +55,13 @@ $(function(){
 
         }
     });
-    $('#mask').click(function(){
-        $('#page,#mask').animate({
-            left:'0'
-        },100);
-        $('#mask').animate({background:'rgba(0,0,0,0)'},100).css({display:'none'});
-   });
+     $('#mask').swipe({
+        swipeStatus:function(event, phase, direction, distance, duration, fingerCount){
+            console.log(event.type);
+        if( event.type == 'touchend'  ){
+                hiddenMenu()
+        }
+    }});
+
 
 });
